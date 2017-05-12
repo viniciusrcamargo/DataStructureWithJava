184d7bbb42c1edf142431a7e3314f2fc9fb3294f
Vinicius Rodrigues Camargo <viniciusr.camargo@gmail.com>
2017-05-12 13:48:16 -0300

    Course of the data structure

---
+++ src/com/vinicius/datastructure/vetor/teste/Aula02.java
@@ -0,0 +1,16 @@
+package com.vinicius.datastructure.vetor.teste;
+
+import com.vinicius.datastructure.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+
+	}
+
+}
