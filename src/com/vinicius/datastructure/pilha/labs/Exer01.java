688cbf06506f5785b4a75058ca06e40b731d6987
Vinicius Rodrigues Camargo <viniciusr.camargo@gmail.com>
2017-06-26 13:15:54 -0300

    Class of the Java Course

--- src/com/vinicius/datastructure/pilha/labs/Exer01.java
+++ src/com/vinicius/datastructure/pilha/labs/Exer01.java
@@ -14,21 +14,33 @@ public class Exer01 {
 		
 		Pilha<Integer> pilha = new Pilha<Integer>();
 		
-		for(int i = 0; i < 2; i++){
-			System.out.println("Entre com o " + (i+1) + " valor");
+		for(int i = 1; i < 10; i++){
+			System.out.println("Entre com um valor");
 			num = scan.nextInt();
 			
 			if(num % 2 == 0){
+				System.out.println("Empilhando o número " + num);
 				pilha.empilha(num);
-			}else{//verificar
-				System.out.println("Desempilhando " + num);
-				pilha.desempilha();
+			}else{
+				Integer desempilhado = pilha.desempilha();
+				
+				if(desempilhado == null){
+					System.out.println("Pilha está vazia");
+				}else{
+					System.out.println("Número impar, desempilhando um número da pilha: " + desempilhado);
+				}
 			}
 		}
+			
+		System.out.println("Todoa os números foram lidos, desempilhando os números");
 		
+		while(!pilha.estaVazia()){
+			
+				System.out.println("Desempilhando um número da pilha: " + pilha.desempilha());
+			
+		}
 		
-		
-		System.out.println(pilha);
+		System.out.println("Todos os elementos foram desempilhados");
 	}
 
 }
